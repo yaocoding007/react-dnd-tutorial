1b17172f70eab86417c2f44134070ce44579bdf7
Mr.yao <lily02@rd.netease.com>
2022-06-15 17:49:59 +0800

    feat: 初始化项目

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        Hello world
+      </header>
+    </div>
+  )
+}
+
+export default App
